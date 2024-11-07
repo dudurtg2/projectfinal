14f837dca6fbced5315d1435ddbbed3114ff0d39
DevSuperMario <devmariomacedo@gmail.com>
2024-11-07 20:51:55 -0300

    Update clientes.php
    Ve ai

--- pages/clientes.php
+++ pages/clientes.php
@@ -45,45 +45,11 @@ include "includes/tables/clientes.php";
                   <input type="text" class="form-control" name="phone" id="inputTelefone" required>
                 </div>
               </div>
-              <div class="col-md-6">
-                <label for="professionSelect" class="form-label">Profissão</label>
-                <div class="input-group input-group-outline">
-                  <select id="professionSelect" class="form-select" name="profession" onchange="toggleOABField()">
-                    <option value="" disabled selected>Selecione a profissão</option>
-                    <?php
-                    $url = 'http://carlo4664.c44.integrator.host:10504/perfis/findAll';
-                    $response = file_get_contents($url);
-                    if ($response !== FALSE) {
-
-                      $data = json_decode($response, true);
-                      if (is_array($data)) {
-                        foreach ($data as $profession) {
-                          echo '<option value="' . htmlspecialchars($profession['id']) . '">' . htmlspecialchars($profession['nome']) . '</option>';
-                        }
-                      }
-                    } else {
-                      echo '<option disabled>Erro ao carregar profissões</option>';
-                    }
-                    ?>
-                  </select>
-                </div>
-              </div>
-              <div class="col-md-6" id="oabField" style="display: none;">
-                <label for="inputOAB" class="form-label">OAB</label>
-                <div class="input-group input-group-outline">
-                  <input type="text" class="form-control" name="oab" id="inputOAB">
-                </div>
-              </div>
-              <div class="col-12">
-                <button type="submit" class="btn btn-primary">Cadastrar</button>
-              </div>
-            </form>
           </div>
-
+        </div>
         </div>
       </div>
     </div>
   </div>
-</div>
 </div>
 <?php
