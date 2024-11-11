fd5abb96299cc6a1cf6ddb82f9e476f77001a4e7
DevSuperMario <devmariomacedo@gmail.com>
2024-11-10 22:05:13 -0300

    mudanças no perfil

--- pages/perfil.php
+++ pages/perfil.php
@@ -13,10 +13,10 @@
           <div class="col-auto my-auto">
             <div class="h-100">
               <h5 class="mb-1">
-                Richard Davis
+                Harvey Specter
               </h5>
               <p class="mb-0 font-weight-normal text-sm">
-                CEO / Co-Founder
+                CEO / Veritas Advocacia
               </p>
             </div>
           </div>
@@ -52,6 +52,7 @@
                 <div class="card-header pb-0 p-3">
                   <h6 class="mb-0">Platform Settings</h6>
                 </div>
+                <!--
                 <div class="card-body p-3">
                   <h6 class="text-uppercase text-body text-xs font-weight-bolder">Account</h6>
                   <ul class="list-group">
@@ -98,6 +99,7 @@
                 </div>
               </div>
             </div>
+
             <div class="col-12 col-xl-4">
               <div class="card card-plain h-100">
                 <div class="card-header pb-0 p-3">
@@ -116,12 +118,13 @@
                   <p class="text-sm">
                     Hi, I’m Alec Thompson, Decisions: If you can’t decide, the answer is no. If two equally difficult paths, choose the one more painful in the short term (pain avoidance is creating an illusion of equality).
                   </p>
+                  -->
                   <hr class="horizontal gray-light my-4">
                   <ul class="list-group">
                     <li class="list-group-item border-0 ps-20 pt-0 text-sm"><strong class="text-dark">Full Name:</strong> &nbsp; Alec M. Thompson</li>
                     <li class="list-group-item border-0 ps-20 text-sm"><strong class="text-dark">Mobile:</strong> &nbsp; (44) 123 1234 123</li>
-                    <li class="list-group-item border-0 ps-20 text-sm"><strong class="text-dark">Email:</strong> &nbsp; alecthompson@mail.com</li>
-                    <li class="list-group-item border-0 ps-20 text-sm"><strong class="text-dark">Location:</strong> &nbsp; USA</li>
+                    <li class="list-group-item border-0 ps-20 text-sm"><strong class="text-dark">Email:</strong> &nbsp; alecthompson@mail.com</li><!--
+                    <li class="list-group-item border-0 ps-20 text-sm"><strong class="text-dark">Location:</strong> &nbsp; USA</li>-->
                     <li class="list-group-item border-0 ps-20 pb-0">
                       <strong class="text-dark text-sm">Social:</strong> &nbsp;
                       <a class="btn btn-facebook btn-simple mb-0 ps-1 pe-2 py-0" href="javascript:;">
